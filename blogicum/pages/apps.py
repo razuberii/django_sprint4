fecf533808478abb4642e3acf031126ef1d8ea08
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:48:35 +0300

    Update apps.py

--- blogicum/pages/apps.py
+++ blogicum/pages/apps.py
@@ -1,6 +1,9 @@
-from django.apps import AppConfig
+from django.apps import AppConfig  # Импортируем класс AppConfig из Django для настройки приложения
 
-
+# Определяем класс конфигурации приложения PagesConfig, который наследует от AppConfig
 class PagesConfig(AppConfig):
+    # Указываем тип поля для автоматического увеличения идентификаторов по умолчанию
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # Задаем имя приложения, которое будет использоваться в Django
     name = 'pages'
