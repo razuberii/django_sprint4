044fe0da53cfdb2ae65355c6688768e966f30122
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:50:31 +0300

    Update apps.py

--- blogicum/users/apps.py
+++ blogicum/users/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig  # Импортируем класс AppConfig для настройки приложения Django
 
 
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+class UsersConfig(AppConfig):  # Определяем класс конфигурации приложения "Users"
+    default_auto_field = 'django.db.models.BigAutoField'  # Указываем тип поля для автоматического увеличения идентификаторов (по умолчанию BigAutoField)
+    name = 'users'  # Задаем имя приложения, которое будет использоваться в проекте
