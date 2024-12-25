1e0d21d66b8baca0efa3c8ec46b8c2c602e013b0
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:51:15 +0300

    Update views.py

--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.contrib.auth import get_user_model  # Импортируем функцию для получения модели пользователя
+from django.contrib.auth.forms import UserCreationForm  # Импортируем форму для создания нового пользователя
+from django.urls import reverse_lazy  # Импортируем функцию для получения URL по имени с отложенной оценкой
+from django.views.generic import CreateView  # Импортируем базовый класс представления для создания объектов
 
-
+# Определяем класс представления для регистрации нового пользователя
 class UserCreateView(CreateView):
-    model = get_user_model()
-    template_name = 'registration/registration_form.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('blog:index')
+    model = get_user_model()  # Устанавливаем модель, с которой будет работать это представление (модель пользователя)
+    template_name = 'registration/registration_form.html'  # Указываем путь к шаблону, который будет использоваться для отображения формы регистрации
+    form_class = UserCreationForm  # Указываем форму, которая будет использоваться для создания нового пользователя
+    success_url = reverse_lazy('blog:index')  # Указываем URL, на который будет перенаправлен пользователь после успешной регистрации (отложенное разрешение имени URL)
