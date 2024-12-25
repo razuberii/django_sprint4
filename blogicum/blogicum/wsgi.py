4dfd146418caf686da10d088c48e883731d8e696
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:47:50 +0300

    Update wsgi.py

--- blogicum/blogicum/wsgi.py
+++ blogicum/blogicum/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os  # Импортируем модуль os для взаимодействия с операционной системой
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # Импортируем функцию для получения WSGI-приложения Django
 
+# Устанавливаем переменную окружения 'DJANGO_SETTINGS_MODULE' в значение 'blogicum.settings'
+# Это указывает Django, какой файл настроек использовать для конфигурации проекта
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogicum.settings')
 
+# Получаем WSGI-приложение Django, которое будет использоваться веб-сервером для обработки запросов
 application = get_wsgi_application()
+
