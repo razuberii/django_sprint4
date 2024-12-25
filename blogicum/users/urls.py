0f2ececdf94c39a455d9428c7a7d086649cf6c36
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:50:54 +0300

    Update urls.py

--- blogicum/users/urls.py
+++ blogicum/users/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path  # Импортируем функцию path для определения маршрутов URL в Django
 
-from . import views
+from . import views  # Импортируем views из текущего приложения для использования представлений
 
+# Определяем список маршрутов URL для приложения
 urlpatterns = [
-    path('', views.UserCreateView.as_view(), name='registration'),
+    path('', views.UserCreateView.as_view(), name='registration'),  # Указываем маршрут для корневого URL, который вызывает представление UserCreateView, 
+    # и задаем ему имя 'registration' для удобства ссылки на него в других частях приложения
 ]
