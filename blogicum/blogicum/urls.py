aee4b3a48c3c49a42f6842df50a1554e2f968cc1
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:46:56 +0300

    Update urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,6 @@
 """blogicum URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
@@ -13,28 +13,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
 
+from django.conf.urls.static import static  # Импортируем функцию для обработки статических файлов
+from django.contrib import admin  # Импортируем административный интерфейс Django
+from django.urls import include, path  # Импортируем функции для маршрутизации URL
+from django.conf import settings  # Импортируем настройки проекта
+
+# Список маршрутов URL для приложения
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('pages/', include('pages.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/', include('users.urls')),
+    path('admin/', admin.site.urls),  # URL для административной панели Django
+    path('', include('blog.urls')),  # Включаем маршруты из приложения 'blog'
+    path('pages/', include('pages.urls')),  # Включаем маршруты из приложения 'pages'
+    path('auth/', include('django.contrib.auth.urls')),  # Включаем маршруты аутентификации от Django
+    path('auth/registration/', include('users.urls')),  # Включаем маршруты регистрации пользователей
 ]
 
+# Обработчик ошибок 403 (доступ запрещен)
 handler403 = 'pages.views.csrf_failure'
 
+# Обработчик ошибок 404 (страница не найдена)
 handler404 = 'pages.views.page_not_found'
 
+# Обработчик ошибок 500 (внутренняя ошибка сервера)
 handler500 = 'pages.views.internal_error'
 
+# Если проект работает в режиме отладки
 if settings.DEBUG:
-    import debug_toolbar
-    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    import debug_toolbar  # Импортируем инструмент отладки
+    # Добавляем маршруты из приложения debug_toolbar к списку urlpatterns
+    urlpatterns += (path('debug/', include(debug_toolbar.urls)),)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Обработка статических файлов и медиафайлов
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Настройка доступа к медиафайлам
