40d2ccf156939f16b9e2428b97c487abc3c918d8
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:48:52 +0300

    Update urls.py

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path  # Импортируем функцию path для определения URL-шаблонов
 
-from . import views
+from . import views  # Импортируем представления (views) из текущего пакета
 
-app_name = 'pages'
+app_name = 'pages'  # Задаем пространство имен для приложения 'pages', чтобы избежать конфликтов имен
 
+# Определяем список URL-шаблонов для приложения 'pages'
 urlpatterns = [
-    path('about/', views.AboutView.as_view(), name='about'),
-    path('rules/', views.RulesView.as_view(), name='rules'),
+    path('about/', views.AboutView.as_view(), name='about'),  # URL для страницы "О нас", связанный с представлением AboutView
+    path('rules/', views.RulesView.as_view(), name='rules'),  # URL для страницы "Правила", связанный с представлением RulesView
 ]
