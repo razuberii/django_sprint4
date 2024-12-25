979200c2ce5792423135c7e23a130f7ebe546cfc
razuberii <nekocatnyashmyash@gmail.com>
2024-12-25 23:53:29 +0300

    Update manage.py

--- blogicum/manage.py
+++ blogicum/manage.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+"""Django's command-line utility for administrative tasks."""  # Докстринг, описывающий назначение этого файла: утилита командной строки Django для административных задач.
 
+import os  # Импортируем модуль os для работы с операционной системой.
+import sys  # Импортируем модуль sys для работы с параметрами командной строки.
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks."""  # Докстринг, описывающий функцию main, которая выполняет административные задачи.
+    
+    # Устанавливаем переменную окружения DJANGO_SETTINGS_MODULE, указывающую на настройки проекта Django.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogicum.settings')
+    
     try:
+        # Импортируем функцию execute_from_command_line из модуля django.core.management.
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        # Если импорт не удался, выбрасываем исключение с сообщением об ошибке.
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from exc  # Указываем оригинальную ошибку, которая привела к ImportError.
+    
+    # Вызываем функцию execute_from_command_line с аргументами командной строки, переданными скрипту.
     execute_from_command_line(sys.argv)
 
-
+# Проверяем, является ли этот файл основным модулем (т.е. запущен ли он напрямую).
 if __name__ == '__main__':
-    main()
+    main()  # Если да, вызываем функцию main для выполнения административных задач.
